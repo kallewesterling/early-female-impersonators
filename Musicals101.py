5a4a8846d93f849ac2163201e883cbbbc9b56a35
Kalle Westerling <KWesterling1@gradcenter.cuny.edu>
2019-11-06 11:56:05 -0500

    also interpreting current theatres

--- Musicals101.py
+++ Musicals101.py
@@ -14,6 +14,10 @@ class ParseTheatreInfo():
         self.later_named = self.parse_later_named()
         self.owners = self.parse_owners()
         self.seats = self.parse_seats()
+        self.references = self.parse_references()
+        self.history = self.parse_history()
+        self.previous_name = self.parse_previous_name()
+        self.later_name = self.parse_later_name()
         
         self.data = {
             'address': self.address,
@@ -24,9 +28,11 @@ class ParseTheatreInfo():
             'later_named': self.later_named,
             'owners': self.owners,
             'seats': self.seats,
+            'references': self.references,
+            'history': self.history,
+            'previous_name': self.previous_name,
+            'later_name': self.later_name,
         }
-        
-        self.reference = self.parse_references()
         
     def parse_address(self):
         g = re.search(r"^(.*)\n", self.text) # first line
@@ -34,22 +40,22 @@ class ParseTheatreInfo():
         else: return(g.groups()[0])
         
     def parse_architect(self):
-        g = re.search(r"Architect: ?(.*)\n", self.text)
+        g = re.search(r"Architect: ?(.*)\n", self.text, re.I)
         if g is None: return(None)
         else: return(g.groups()[0])
         
     def parse_built(self):
-        g = re.search(r"Built: ?(.*)\n", self.text)
+        g = re.search(r"Built: ?(.*)\n", self.text, re.I)
         if g is None: return(None)
         else: return(g.groups()[0])
         
     def parse_demolished(self):
-        g = re.search(r"Demolished: ?(.*)\n", self.text)
+        g = re.search(r"Demolished: ?(.*)\n", self.text, re.I)
         if g is None: return(None)
         else: return(g.groups()[0])
         
     def parse_original_name(self):
-        g = re.search(r"Original n|Name: ?(.*)\n", self.text)
+        g = re.search(r"Original n|Name: ?(.*)\n", self.text, re.I)
         if g is None: return(None)
         else: return(g.groups()[0])
         
@@ -59,12 +65,12 @@ class ParseTheatreInfo():
         else: return(g.groups()[0])
         
     def parse_owners(self):
-        g = re.search(r"Owners: ?(.*)\n", self.text)
+        g = re.search(r"(Owners|Owners/Managers): ?(.*)\n", self.text, re.I)
         if g is None: return(None)
-        else: return(g.groups()[0])
+        else: return(g.groups()[1])
         
     def parse_seats(self):
-        g = re.search(r"Seats: ?(.*)\n", self.text)
+        g = re.search(r"Seats: ?(.*)\n", self.text, re.I)
         if g is None: return(None)
         else:
             seats = g.groups()[0]
@@ -77,6 +83,21 @@ class ParseTheatreInfo():
             return(seats)
         
     def parse_references(self):
-        g = re.search(r"- see (.*)", self.text)
+        g = re.search(r"- see (.*)", self.text, re.I)
+        if g is None: return(None)
+        else: return(g.groups()[0])
+        
+    def parse_history(self):
+        g = re.search(r"History: ?(.*)\n", self.text, re.I)
+        if g is None: return(None)
+        else: return(g.groups()[0])
+        
+    def parse_previous_name(self):
+        g = re.search(r"Previous Names?: ?(.*)\n", self.text, re.I)
+        if g is None: return(None)
+        else: return(g.groups()[0])
+        
+    def parse_later_name(self):
+        g = re.search(r"Later Names?: ?(.*)\n", self.text, re.I)
         if g is None: return(None)
         else: return(g.groups()[0])
